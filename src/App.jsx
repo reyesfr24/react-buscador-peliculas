c9782fbf2c376c31bce5dfb9f74ac5e207724b66
Francisco Reyes <130443772+reyesfr24@users.noreply.github.com>
2025-01-27 09:45:22 +0100

    Forma correcta y CONTROLADA de manejar un formulario con React

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,50 @@ function App () {
   const { movies } = useMovies() //                           Custom Hook return { movies: mappedMovies }
   // const inputRef = useRef()
   const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  const handleSubmit = (event) => { //                        Este método se ejecuta al pulsar el botón
+    event.preventDefault() //                                 Al pasar el "event" nos permite bloquar que recargue la pagina utilizando "event.preventDefault()" cuando se dispara el evento
     console.log({ query })
   }
-  const handleChange = (event) => {
-    setQuery(event.target.value)
+
+  const handleChange = (event) => { //                        Se ejecuta cada vez que cambia el valor del texto
+  //                                                          Esta función está asociada al input por la propiedad onChange que dispara la función cada vez que cambia la información
+    const newQuery = event.target.value //                    "event.target" es una referencia al elemento que disparó el evento (<input> en este caso)
+    setQuery(newQuery)
+
+    if (query === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (query.match(/^\d+$/)) {
+      setError('No se puede buscar una película con una número')
+      return
+    }
+
+    if (query.length < 3) {
+      setError('La búsqueda debe tener al menos tres caracteres')
+      return
+    }
+
+    setError(null)
   }
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de películas</h1>
-        <div className='linea'>
-          <form className='form' onSubmit={handleSubmit}>
-            <input onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
-            <button type='submit'>Buscar</button>
-          </form>
-        </div>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange} //                       Asocia el cambio en el input a la función
+            value={query} //                                 La propiedad "value={query}" se utiliza para hacer que el campo de texto sea controlado,
+            name='query'//                                   asegura que el valor visible del campo de texto siempre coincida con el valor del estado query
+            placeholder='Avengers, Star Wars, The Matrix...'
+          />
+          <button type='submit'>Buscar</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
