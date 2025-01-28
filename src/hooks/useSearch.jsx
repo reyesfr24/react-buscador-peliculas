6643617603f197e2a4b7db318ff0f5c0b46de715
Francisco Reyes <130443772+reyesfr24@users.noreply.github.com>
2025-01-28 09:07:17 +0100

    Añade useRef para controlar si es la primera ejecuccionÃ

--- src/hooks/useSearch.jsx
+++ src/hooks/useSearch.jsx
@@ -1,10 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export function useSearch () {
   const [search, updateSearch] = useState('')
   const [error, setError] = useState(null)
+  const isFirstInput = useRef(true) //                               Como el valor del useRef no cambia al renderizar vamos a utilizarlo para saber si es la primera interacción
 
   useEffect(() => {
+    if (isFirstInput.current) { //                                   Con el useRef necesitamos el current para modificar el valor de la referencia
+      isFirstInput.current = search === '' //                        Asigna el resultado de la comparación search === ''
+      return //                                                      Cuando el search tenga contenido "isFirstInput.current" se fijará como false y la próxima vez que cambie el search ya no entrará en este if
+    } //                                                             Este return hace que si se ejecuta el if, al llegar al return para la ejecución del efecto entero
+
     if (search === '') {
       setError('No se puede buscar una película vacía')
       return
